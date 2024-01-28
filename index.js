5afcb7fd64c0f8846a867cb18f94fe953b4347cd
gabrielRobless <rgabrielsalvador1@gmail.com>
2024-01-27 19:08:18 -0600

    agencia de viajes con node

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+const app = express();
+
+// conectar base de datos 
+db.authenticate()
+    .then(()=> console.log('Base de datos conectada '))
+    .catch( error => console.log(error));
+
+
+// definir el pruerto 
+const port = process.env.PORT || 4000;
+
+// Habilitar pug
+app.set('view engine', 'pug');
+
+// Obtener fecha actual
+app.use((req, res, next)=>{
+    const year = new Date();
+
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombresitio = "Agencia de Viajes";
+    
+  return  next();
+});
+
+// Agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+
+// Definir carpeta publica
+app.use(express.static('public'));
+
+//  Agregar Router
+app.use('/', router);
+
+app.listen(port, () =>[
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+]) 
